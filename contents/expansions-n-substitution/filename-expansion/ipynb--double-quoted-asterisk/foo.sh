a0978589f00620ef1f36663bbf54fe5123aed679
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2024-06-07 21:27:49 +0800

    added: demo

--- contents/expansions-n-substitution/filename-expansion/ipynb--double-quoted-asterisk/foo.sh
+++ contents/expansions-n-substitution/filename-expansion/ipynb--double-quoted-asterisk/foo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-echo "this asterisk is not double-quoted:" b*.txt
-echo "this asterisk is double-quoted: a*.txt" 
+echo "this asterisk is not double-quoted:" **/b*.txt
+echo "this asterisk is double-quoted: **/b*.txt" 
 echo "this asterisk is double-quoted and preceded with a dollar sign: $*" 
